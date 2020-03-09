3f7f100c0848be78dea4010da3d474659ffa9fba
Bruno Clermont <bruno@robotinfra.com>
2020-03-09 22:01:35 +0800

    only create obj if crd is there

--- deploy/package.sh
+++ deploy/package.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-docker run --rm -ti -v `pwd`:/workdir --workdir=/workdir alpine/helm:3.0.3 package .
+docker run --rm -ti -v `pwd`:/workdir --workdir=/workdir alpine/helm:3.1.1 package .

c583402fcc77c4396439a27316a77f5dfc491974
Björn Domars <bdomars@walkbase.com>
2020-11-12 11:38:27 +0200

    Remove devcontainer and vscode tasks

--- .devcontainer/k8s_cleanup.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-kubectl delete -f deploy/crds/minio.walkbase.com_miniobuckets_crd.yaml
-kubectl delete -f deploy/crds/minio.walkbase.com_miniousers_crd.yaml
-kubectl delete -f deploy/crds/minio.walkbase.com_minioservers_crd.yaml
--- .devcontainer/k8s_setup.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-set -e
-
-kubectl create -f deploy/crds/minio.walkbase.com_miniousers_crd.yaml
-kubectl create -f deploy/crds/minio.walkbase.com_miniobuckets_crd.yaml
-kubectl create -f deploy/crds/minio.walkbase.com_minioservers_crd.yaml

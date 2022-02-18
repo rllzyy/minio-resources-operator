546b95606fafc20ef158cb7c3a9ee4589268b021
Björn Domars <bdomars@walkbase.com>
2022-02-18 14:11:57 +0200

    Fresh start

--- pkg/apis/minio/v1alpha1/minioserver.go
+++
@@ -1,8 +0,0 @@
-package v1alpha1
-
-import "fmt"
-
-// GetHostname return a minio client compatible hostname
-func (ms *MinioServerSpec) GetHostname() string {
-	return fmt.Sprintf("%s:%d", ms.Hostname, ms.Port)
-}
--- version/version.go
+++
@@ -1,6 +0,0 @@
-package version
-
-var (
-	// Version of operator
-	Version = "0.4.0"
-)

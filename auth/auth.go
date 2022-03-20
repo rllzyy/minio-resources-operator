9f249080e5de8d2a1366783fd8677069084347ba
Björn Domars <bdomars@walkbase.com>
2022-03-20 20:54:33 +0200

    add function to generate credentials

--- auth/auth.go
+++ auth/auth.go
@@ -1,10 +1,61 @@
 package auth
 
+import (
+	"math/rand"
+	"strings"
+	"time"
+)
+
+var (
+	lowerCharSet   = "abcdedfghijklmnopqrst"
+	upperCharSet   = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+	specialCharSet = "!@#$%&*"
+	numberSet      = "0123456789"
+	allCharSet     = lowerCharSet + upperCharSet + specialCharSet + numberSet
+)
+
+func generatePassword(passwordLength, minSpecialChar, minNum, minUpperCase int) string {
+	var password strings.Builder
+
+	rand.Seed(time.Now().Unix())
+	//Set special character
+	for i := 0; i < minSpecialChar; i++ {
+		random := rand.Intn(len(specialCharSet))
+		password.WriteString(string(specialCharSet[random]))
+	}
+
+	//Set numeric
+	for i := 0; i < minNum; i++ {
+		random := rand.Intn(len(numberSet))
+		password.WriteString(string(numberSet[random]))
+	}
+
+	//Set uppercase
+	for i := 0; i < minUpperCase; i++ {
+		random := rand.Intn(len(upperCharSet))
+		password.WriteString(string(upperCharSet[random]))
+	}
+
+	remainingLength := passwordLength - minSpecialChar - minNum - minUpperCase
+	for i := 0; i < remainingLength; i++ {
+		random := rand.Intn(len(allCharSet))
+		password.WriteString(string(allCharSet[random]))
+	}
+	inRune := []rune(password.String())
+	rand.Shuffle(len(inRune), func(i, j int) {
+		inRune[i], inRune[j] = inRune[j], inRune[i]
+	})
+	return string(inRune)
+}
+
 type Credentials struct {
 	AccessKey string
 	SecretKey string
 }
 
 func GetNewCredentials() (Credentials, error) {
-	return Credentials{}, nil
+	return Credentials{
+		AccessKey: generatePassword(16, 0, 4, 8),
+		SecretKey: generatePassword(16, 2, 4, 8),
+	}, nil
 }
